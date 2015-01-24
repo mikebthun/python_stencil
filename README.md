eab05f1339d8020b20079c2c1cd7f96982b979ca
Mike B <mike@masslobby.com>
2015-01-24 08:32:04 -0800

    Added stencil and updated README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # python_stencil
+
 Python console application template with command line arguments
+
+Includes logging, help function and option -v changing logging to debug.
+
+## License
+
+Apache License 2.0
+
+
+## Contributors
+
+mikebthun
+
+
+
---
+++ stencil.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python -tt
+
+import sys,getopt
+ 
+import logging
+
+logger = logging.getLogger('stencil')
+hdlr = logging.StreamHandler(sys.stdout)
+#hdlr = logging.FileHandler('stencil.log') 
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+logger.setLevel(logging.INFO) #logging.DEBUG
+
+
+def help():
+
+  print " Usage: stencil.py --help "
+
+ 
+def main(argv):
+
+  # make sure command line arguments are valid
+
+
+  try:
+    options, args = getopt.getopt(
+
+       argv, 
+      'hv', 
+      [ 
+        'help',
+        'verbose' 
+    
+      ])
+ 
+  except getopt.GetoptError:
+    logging.fatal("Bad options!")
+    help()
+    sys.exit(2)
+
+
+  # handle command line arugments
+  for opt, arg in options:
+    if opt in ('-h', '--help'):
+      help()
+      sys.exit(2)
+    elif opt in ('-v', '--verbose'):
+      logger.setLevel(logging.DEBUG) 
+ 
+
+  ###################################
+  # main code starts here
+  ###################################
+
+  logger.info('Hello')
+  logger.debug('Hello')
+ 
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
+ 
+ 
